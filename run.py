4292ef95fa7cc9a2769a86e28405d53f02f390fd
EggPotato-ops <jiajing.lim@tsgs.com>
2024-11-05 21:55:41 +0800

    remove all that cannot run in streamlit

--- run.py
+++ run.py
@@ -2,8 +2,6 @@
 import streamlit as st
 from pathlib import Path
 from modules.photovideo import PhotoVideoProcessor
-from modules.webcam import WebcamDetector
-#from modules.screencapture import ScreenMonitor
 from modules.telegram import TelegramConfig
 from modules.logger import logger
 
@@ -21,7 +19,7 @@ telegram.configure_sidebar()
 # Sidebar: Choose detection mode
 mode = st.sidebar.selectbox(
     "Choose a Detection Mode:",
-    ["Upload Photo/Video", "Webcam Realtime Detection"]#, "Screen Monitoring"]
+    ["Upload Photo/Video"]
 )
 
 # Get Base Directory
@@ -31,11 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent
 if mode == "Upload Photo/Video":
     processor = PhotoVideoProcessor(BASE_DIR)
     processor.process()
-elif mode == "Webcam Realtime Detection":
-    detector = WebcamDetector(BASE_DIR)
-    detector.start_detection()
-# elif mode == "Screen Monitoring":
-#     monitor = ScreenMonitor(BASE_DIR)
-#     monitor.start_monitoring()
+
 
 logger.info("Application ended.")
